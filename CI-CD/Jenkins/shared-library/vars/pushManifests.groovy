1223b2ad9686e1bddbb8f95d034d90e913db8a80
ayatmoatamed <ayatmoatamed05@gmail.com>
2026-08-16 19:07:19 +0200

    Fix GitHub authentication for manifest push

--- CI-CD/Jenkins/shared-library/vars/pushManifests.groovy
+++ CI-CD/Jenkins/shared-library/vars/pushManifests.groovy
@@ -1,13 +1,24 @@
 def call(Map args = [:]) {
 
-    def commitMessage = args.get(
-        'commitMessage',
-        'Update Kubernetes image'
-    )
+    def repoUrl = args.repoUrl
+    def branch = args.get('branch', 'main')
+    def commitMessage = args.get('commitMessage', 'Update Kubernetes image')
+    def credentialsId = args.credentialsId
+    def paths = args.get('paths', '.')
 
-    sh """
-        git add Kubernetes/
-        git commit -m "${commitMessage}" || true
-        git push origin HEAD
-    """
+    withCredentials([usernamePassword(
+        credentialsId: credentialsId,
+        usernameVariable: 'GIT_USER',
+        passwordVariable: 'GIT_TOKEN'
+    )]) {
+        def repoNoScheme = repoUrl.replaceFirst('https://', '').replaceFirst('\\.git$', '')
+
+        sh """
+            git config user.email "jenkins@ci.local"
+            git config user.name "Jenkins CI"
+            git add ${paths}
+            git commit -m "${commitMessage}" || true
+            git push https://\\\${GIT_USER}:\\\${GIT_TOKEN}@${repoNoScheme}.git HEAD:${branch}
+        """
+    }
 }
