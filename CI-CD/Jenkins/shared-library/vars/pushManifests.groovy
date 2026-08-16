09aeb06c14ab06dda47426a16ff1224db043d1b6
ayatmoatamed <ayatmoatamed05@gmail.com>
2026-08-16 19:12:45 +0200

    fix pushManifests.groovy

--- CI-CD/Jenkins/shared-library/vars/pushManifests.groovy
+++ CI-CD/Jenkins/shared-library/vars/pushManifests.groovy
@@ -1,24 +1,37 @@
+```groovy
 def call(Map args = [:]) {
 
     def repoUrl = args.repoUrl
     def branch = args.get('branch', 'main')
-    def commitMessage = args.get('commitMessage', 'Update Kubernetes image')
+    def commitMessage = args.get(
+        'commitMessage',
+        'Update Kubernetes image'
+    )
     def credentialsId = args.credentialsId
     def paths = args.get('paths', '.')
 
-    withCredentials([usernamePassword(
-        credentialsId: credentialsId,
-        usernameVariable: 'GIT_USER',
-        passwordVariable: 'GIT_TOKEN'
-    )]) {
-        def repoNoScheme = repoUrl.replaceFirst('https://', '').replaceFirst('\\.git$', '')
+    withCredentials([
+        usernamePassword(
+            credentialsId: credentialsId,
+            usernameVariable: 'GIT_USER',
+            passwordVariable: 'GIT_TOKEN'
+        )
+    ]) {
+
+        def repoNoScheme = repoUrl
+            .replaceFirst('https://', '')
+            .replaceFirst('\\.git$', '')
 
         sh """
             git config user.email "jenkins@ci.local"
             git config user.name "Jenkins CI"
+
             git add ${paths}
+
             git commit -m "${commitMessage}" || true
-            git push https://\\\${GIT_USER}:\\\${GIT_TOKEN}@${repoNoScheme}.git HEAD:${branch}
+
+            git push "https://\\${GIT_USER}:\\${GIT_TOKEN}@${repoNoScheme}.git" HEAD:${branch}
         """
     }
 }
+```
